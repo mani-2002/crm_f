799f6e3c796d7ab5b2901eb5b4cd8be8437237b6
mani2002 <manikantavinjamuri8522@gmail.com>
2024-07-31 23:22:34 +0530

    'added_message_input_for_user'

--- src/pages/UserDashboard.js
+++ src/pages/UserDashboard.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 const UserDashboard = () => {
   const navigate = useNavigate();
@@ -17,6 +19,10 @@ const UserDashboard = () => {
     navigate("/");
   };
 
+  const [message, setMessage] = useState("");
+  const sendMessage = (e) => {
+    alert(message);
+  };
   return (
     <div
       style={{
@@ -63,7 +69,69 @@ const UserDashboard = () => {
           <div
             style={{ border: "1px solid black", height: "45vh", width: "50vh" }}
           >
-            chatbox
+            <div style={{ height: "37.5vh" }}>1</div>
+            <div
+              style={{
+                height: "7.5vh",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                borderTop: "1px solid black",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                }}
+              >
+                <div
+                  style={{
+                    width: "84%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <input
+                    className="form-control"
+                    placeholder="Enter Your Message Here..."
+                    style={{
+                      borderRadius: "0 0 0 0",
+                      height: "6.8vh",
+                      border: "none",
+                    }}
+                    onChange={(e) => {
+                      setMessage(e.target.value);
+                    }}
+                  />
+                </div>
+                <div
+                  style={{
+                    width: "16%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <button className="btn" onClick={sendMessage}>
+                    <FontAwesomeIcon
+                      icon={faPaperPlane}
+                      style={{
+                        color: "#ffffff",
+                        borderRadius: "50%",
+                        backgroundColor: "green",
+                        padding: "2vh",
+                        marginTop: "1px",
+                      }}
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
